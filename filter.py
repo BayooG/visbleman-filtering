8e4c71d36596119b492900921289ddd354118d6e
obay daba <obaydaba96@gmail.com>
2020-01-23 22:20:14 +0200

    use contours to substarct the ice

--- filter.py
+++ filter.py
@@ -9,18 +9,13 @@ def detect_pictures(directory):
             filter_image(pic)
 
 
-def filter_image(path):
-
-    k = 3
+def filter_image(path, clusers = 3):
+    k = clusers
     kernel = np.ones((9,9),np.uint8)
-    
     img = cv2.imread(path)
-
-
     Z = img.reshape((-1,3))
     # convert to np.float32
     Z = np.float32(Z)
-
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     ret,label,center=cv2.kmeans(Z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
@@ -28,14 +23,19 @@ def filter_image(path):
     res = center[label.flatten()]
     res2 = res.reshape((img.shape))
     gray = cv2.cvtColor(res2, cv2.COLOR_BGR2GRAY)
-
     closing = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel,iterations=1)
     opening = cv2.morphologyEx(closing, cv2.MORPH_CLOSE, kernel,iterations=1)
-
     _, thresh = cv2.threshold(opening, 170, 255, cv2.THRESH_BINARY)
+    contours, _  = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = sorted(contours, key= cv2.contourArea, reverse=True)[:2]
+    cv2.fillPoly(gray, pts =contours, color=(255,255,255))
+    cv2.imshow(" ", gray)
+    _, thresh = cv2.threshold(gray, 254, 255, cv2.THRESH_BINARY)
     res = cv2.bitwise_and(img,img,mask = thresh)
-    cv2.imshow('closing',res)
-
+    
+    #should be saved 
+    cv2.imshow('final',res)
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
